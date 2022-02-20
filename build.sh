9228fd101572da7e24e87928ff2a1a717809ae40
TheFabiCraft <44499040+fabianneufang@users.noreply.github.com>
2022-02-20 15:16:51 +0100

    Build shell

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
+#!/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/usr/lib/jvm/java-16-oracle/bin:/usr/lib/jvm/java-16-oracle/db/bin
-
 docker-compose build
 docker-compose down
 docker-compose up -d
